a29ae18f49b3cdcfc802edce9eaa0d8341f77609
Scott Hoisington <hoyzington@gmail.com>
2019-11-05 03:09:12 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -11,9 +11,10 @@ def join_ingredients(src)
   pizza = []
   inner_array = 0
   while inner_array < src.count do
-    pizza[inner_array] = "I love #{src[inner_array][0]} and #{src[array][1]} on my pizza"
+    pizza[inner_array] = "I love #{src[inner_array][0]} and #{src[inner_array][1]} on my pizza"
     array += 1
   end
+  pizza
 end
 
 def find_greater_pair(src)
